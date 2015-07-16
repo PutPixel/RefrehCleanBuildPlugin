5ed55c12fd40f7c088df9a68a80c8c19b69509e2
andrei.sviridenko <putpix@gmail.com>
2015-07-16 12:58:32 +0200

    Remove unused imports

--- RefrehCleanBuild/src/com/github/putpixel/refrehcleanbuild/RefreshCleanBuildCommand.java
+++ RefrehCleanBuild/src/com/github/putpixel/refrehcleanbuild/RefreshCleanBuildCommand.java
@@ -17,7 +17,6 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.jdt.core.IJavaModelMarker;
 import org.eclipse.ui.progress.IProgressConstants2;
 
 import refrehcleanbuild.Activator;
